e09fe220370ccb503362d11c47a63e9a1e96e752
John T. Wodder II <git@varonathe.org>
2022-11-29 17:05:16 -0500

    Solved 2018-09a!

---
+++ 2018/09a/src/main.rs
@@ -0,0 +1,64 @@
+use adventutil::pullparser::{ParseError, PullParser, Token};
+use adventutil::Input;
+use std::str::FromStr;
+
+struct Game {
+    players: usize,
+    last_marble: usize,
+}
+
+impl Game {
+    fn high_score(&self) -> usize {
+        let mut scores = vec![0; self.players];
+        let mut circle = vec![0];
+        let mut current = 0;
+        for (player, marble) in (0..self.players).cycle().zip(1..=self.last_marble) {
+            if marble % 23 == 0 {
+                scores[player] += marble;
+                current = (current + circle.len() * 7 - 7) % circle.len();
+                scores[player] += circle.remove(current);
+            } else {
+                current = (current + 2) % circle.len();
+                circle.insert(current, marble);
+            }
+        }
+        scores.into_iter().max().unwrap()
+    }
+}
+
+impl FromStr for Game {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Game, ParseError> {
+        let mut parser = PullParser::new(s.trim());
+        let players = parser.parse_to::<usize, _>(Token::Whitespace)?;
+        parser.skip("players; last marble is worth ")?;
+        let last_marble = parser.parse_to::<usize, _>(Token::Whitespace)?;
+        parser.skip("points")?;
+        parser.eof()?;
+        Ok(Game {
+            players,
+            last_marble,
+        })
+    }
+}
+
+fn main() {
+    println!("{}", Input::from_env().parse::<Game>().high_score());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("10 players; last marble is worth 1618 points", 8317)]
+    #[case("13 players; last marble is worth 7999 points", 146373)]
+    #[case("17 players; last marble is worth 1104 points", 2764)]
+    #[case("21 players; last marble is worth 6111 points", 54718)]
+    #[case("30 players; last marble is worth 5807 points", 37305)]
+    fn test_high_score(#[case] game: Game, #[case] score: usize) {
+        assert_eq!(game.high_score(), score);
+    }
+}
