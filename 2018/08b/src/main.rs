1e070e8f9788845830ff87233e7809e6e082eb8a
John T. Wodder II <git@varonathe.org>
2022-11-29 12:25:43 -0500

    Solved 2018-08b!

---
+++ 2018/08b/src/main.rs
@@ -0,0 +1,112 @@
+use adventutil::Input;
+use std::num::ParseIntError;
+use std::str::SplitWhitespace;
+use thiserror::Error;
+
+struct Node {
+    #[allow(unused)]
+    children: Vec<Node>,
+    #[allow(unused)]
+    metadata: Vec<usize>,
+    value: usize,
+}
+
+impl Node {
+    fn new(children: Vec<Node>, metadata: Vec<usize>) -> Node {
+        let value = if children.is_empty() {
+            metadata.iter().copied().sum()
+        } else {
+            metadata
+                .iter()
+                .filter_map(|&i| {
+                    (i != 0).then(|| match children.get(i - 1) {
+                        Some(n) => n.value,
+                        None => 0,
+                    })
+                })
+                .sum()
+        };
+        Node {
+            children,
+            metadata,
+            value,
+        }
+    }
+
+    fn parse_tree(s: &str) -> Result<Node, ParseError> {
+        let mut stream = NumberStream::new(s);
+        let node = Node::parse_node(&mut stream)?;
+        stream.at_end()?;
+        Ok(node)
+    }
+
+    fn parse_node(stream: &mut NumberStream<'_>) -> Result<Node, ParseError> {
+        let child_qty = stream.get()?;
+        let meta_qty = stream.get()?;
+        let mut children = Vec::with_capacity(child_qty);
+        for _ in 0..child_qty {
+            children.push(Node::parse_node(stream)?);
+        }
+        let mut metadata = Vec::with_capacity(meta_qty);
+        for _ in 0..meta_qty {
+            metadata.push(stream.get()?);
+        }
+        Ok(Node::new(children, metadata))
+    }
+}
+
+struct NumberStream<'a> {
+    inner: SplitWhitespace<'a>,
+}
+
+impl<'a> NumberStream<'a> {
+    fn new(s: &'a str) -> Self {
+        NumberStream {
+            inner: s.split_whitespace(),
+        }
+    }
+
+    fn get(&mut self) -> Result<usize, ParseError> {
+        Ok(self
+            .inner
+            .next()
+            .ok_or(ParseError::Short)?
+            .parse::<usize>()?)
+    }
+
+    fn at_end(&mut self) -> Result<(), ParseError> {
+        if self.inner.next().is_none() {
+            Ok(())
+        } else {
+            Err(ParseError::Trailing)
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+enum ParseError {
+    #[error("Input had too few components")]
+    Short,
+    #[error("Input had trailing components")]
+    Trailing,
+    #[error("Invalid integer: {0}")]
+    InvalidInt(#[from] ParseIntError),
+}
+
+fn root_value(s: &str) -> usize {
+    Node::parse_tree(s).unwrap().value
+}
+
+fn main() {
+    println!("{}", root_value(&Input::from_env().read()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert_eq!(root_value("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"), 66);
+    }
+}
