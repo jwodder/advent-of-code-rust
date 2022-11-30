bc786d71bb0cfd1af00a847ffcc2cba208dc8a90
John T. Wodder II <git@varonathe.org>
2022-11-30 13:41:12 -0500

    Solved 2015-08b!

---
+++ 2015/08b/src/main.rs
@@ -0,0 +1,30 @@
+use adventutil::Input;
+
+fn repr_delta(s: &str) -> usize {
+    2 + s.chars().filter(|&c| c == '"' || c == '\\').count()
+}
+
+fn main() {
+    println!(
+        "{}",
+        Input::from_env()
+            .lines()
+            .map(|s| repr_delta(&s))
+            .sum::<usize>()
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(r#""""#, 4)]
+    #[case(r#""abc""#, 4)]
+    #[case(r#""aaa\"aaa""#, 6)]
+    #[case(r#""\x27""#, 5)]
+    fn test_repr_delta(#[case] s: &str, #[case] delta: usize) {
+        assert_eq!(repr_delta(s), delta);
+    }
+}
