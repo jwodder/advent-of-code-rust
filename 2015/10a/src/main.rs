5ed4b8d477994436aa6b6c1f21ce22c8f454ea5f
John T. Wodder II <git@varonathe.org>
2022-11-30 16:22:42 -0500

    Solved 2015-10a!

---
+++ 2015/10a/src/main.rs
@@ -0,0 +1,37 @@
+use adventutil::Input;
+use itertools::Itertools;
+
+fn look_and_say(s: &str) -> String {
+    s.chars()
+        .group_by(|&c| c)
+        .into_iter()
+        .map(|(c, run)| format!("{}{}", run.count(), c))
+        .collect()
+}
+
+fn solve(mut s: String) -> usize {
+    for _ in 0..40 {
+        s = look_and_say(s.trim());
+    }
+    s.len()
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env().read()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("1", "11")]
+    #[case("11", "21")]
+    #[case("21", "1211")]
+    #[case("1211", "111221")]
+    #[case("111221", "312211")]
+    fn test_look_and_say(#[case] before: &str, #[case] after: &str) {
+        assert_eq!(look_and_say(before), after);
+    }
+}
