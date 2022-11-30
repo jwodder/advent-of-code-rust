2f155ab80a6b6586c44d28c27f003ea36c280916
John T. Wodder II <git@varonathe.org>
2022-11-30 13:22:39 -0500

    Solved 2015-08a!

---
+++ 2015/08a/src/main.rs
@@ -0,0 +1,47 @@
+use adventutil::Input;
+
+fn repr_delta(s: &str) -> usize {
+    let mut iter = s.chars();
+    let mut delta = 0;
+    while let Some(c) = iter.next() {
+        match c {
+            '"' => delta += 1,
+            '\\' => match iter.next().unwrap() {
+                '"' | '\\' => delta += 1,
+                'x' => {
+                    let _ = iter.next().unwrap();
+                    let _ = iter.next().unwrap();
+                    delta += 3;
+                }
+                c => panic!("Invalid escape sequence \\{c}"),
+            },
+            _ => (),
+        }
+    }
+    delta
+}
+
+fn main() {
+    println!(
+        "{}",
+        Input::from_env()
+            .lines()
+            .map(|s| repr_delta(&s))
+            .sum::<usize>()
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(r#""""#, 2)]
+    #[case(r#""abc""#, 2)]
+    #[case(r#""aaa\"aaa""#, 3)]
+    #[case(r#""\x27""#, 5)]
+    fn test_repr_delta(#[case] s: &str, #[case] delta: usize) {
+        assert_eq!(repr_delta(s), delta);
+    }
+}
