98ddc120cc8726d7cffc32ff07870c4a027bd5aa
John T. Wodder II <git@varonathe.org>
2022-12-01 17:18:36 -0500

    Solved 2020-01a!

---
+++ 2020/01a/src/main.rs
@@ -0,0 +1,28 @@
+use adventutil::Input;
+
+fn solve(input: Input) -> u32 {
+    let entries = input.parse_lines::<u32>().collect::<Vec<_>>();
+    for i in 0..entries.len() {
+        for j in (i + 1)..entries.len() {
+            if entries[i] + entries[j] == 2020 {
+                return entries[i] * entries[j];
+            }
+        }
+    }
+    panic!("No solution found");
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from("1721\n979\n366\n299\n675\n1456");
+        assert_eq!(solve(input), 514579);
+    }
+}
