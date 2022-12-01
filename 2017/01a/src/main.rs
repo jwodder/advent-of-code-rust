901632c38295157601372ade5579b45dc8b749ec
John T. Wodder II <git@varonathe.org>
2022-12-01 17:34:48 -0500

    Solved 2017-01a!

---
+++ 2017/01a/src/main.rs
@@ -0,0 +1,34 @@
+use adventutil::Input;
+
+fn solve(input: Input) -> u32 {
+    let digits = input
+        .read()
+        .trim()
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .collect::<Vec<_>>();
+    digits
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &c)| (c == digits[(i + 1) % digits.len()]).then_some(c))
+        .sum()
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("1122", 3)]
+    #[case("1111", 4)]
+    #[case("1234", 0)]
+    #[case("91212129", 9)]
+    fn test_solve(#[case] s: &'static str, #[case] total: u32) {
+        assert_eq!(solve(Input::from(s)), total);
+    }
+}
