4f9fadcd0e0157f0543426fb659e2ffa92846bea
John T. Wodder II <git@varonathe.org>
2022-12-02 14:53:58 -0500

    Solved 2020-06b!

---
+++ 2020/06b/src/main.rs
@@ -0,0 +1,33 @@
+use adventutil::Input;
+use std::collections::HashSet;
+
+fn solve(input: Input) -> usize {
+    input
+        .paragraphs()
+        .map(|para| {
+            para.lines()
+                .map(|s| s.chars().collect::<HashSet<char>>())
+                .reduce(|accum, newset| accum.intersection(&newset).copied().collect())
+                .unwrap()
+                .len()
+        })
+        .sum()
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from(concat!(
+            "abc\n", "\n", "a\n", "b\n", "c\n", "\n", "ab\n", "ac\n", "\n", "a\n", "a\n", "a\n",
+            "a\n", "\n", "b\n",
+        ));
+        assert_eq!(solve(input), 6);
+    }
+}
