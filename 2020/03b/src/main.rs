4ece7b1b311302b152d2cf72f573e344e78f47a7
John T. Wodder II <git@varonathe.org>
2022-12-01 21:33:19 -0500

    Solved 2020-03b!

---
+++ 2020/03b/src/main.rs
@@ -0,0 +1,55 @@
+use adventutil::grid::Grid;
+use adventutil::Input;
+
+fn solve(input: Input) -> u64 {
+    let grid = input.parse::<Grid<char>>().map(|c| c == '#');
+    [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .into_iter()
+        .map(|(right, down)| intercepts(&grid, right, down))
+        .product()
+}
+
+fn intercepts(grid: &Grid<bool>, right: usize, down: usize) -> u64 {
+    let mut trees = 0;
+    let mut cell = grid.get_cell((0, 0));
+    while let Some(mut pos) = cell {
+        if *pos.get() {
+            trees += 1;
+        }
+        for _ in 0..right {
+            pos = pos.east_wrap();
+        }
+        cell = Some(pos);
+        for _ in 0..down {
+            cell = cell.and_then(|pos| pos.south());
+        }
+    }
+    trees
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from(concat!(
+            "..##.......\n",
+            "#...#...#..\n",
+            ".#....#..#.\n",
+            "..#.#...#.#\n",
+            ".#...##..#.\n",
+            "..#.##.....\n",
+            ".#.#.#....#\n",
+            ".#........#\n",
+            "#.##...#...\n",
+            "#...##....#\n",
+            ".#..#...#.#\n",
+        ));
+        assert_eq!(solve(input), 336);
+    }
+}
