00b29ed06f06e4f467674430f05909e1ee727adf
John T. Wodder II <git@varonathe.org>
2022-12-01 21:26:21 -0500

    Solved 2020-03a!

---
+++ 2020/03a/src/main.rs
@@ -0,0 +1,42 @@
+use adventutil::grid::Grid;
+use adventutil::Input;
+
+fn solve(input: Input) -> usize {
+    let grid = input.parse::<Grid<char>>().map(|c| c == '#');
+    let mut trees = 0;
+    let mut cell = grid.get_cell((0, 0));
+    while let Some(pos) = cell {
+        if *pos.get() {
+            trees += 1;
+        }
+        cell = pos.east_wrap().east_wrap().east_wrap().south();
+    }
+    trees
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from(concat!(
+            "..##.......\n",
+            "#...#...#..\n",
+            ".#....#..#.\n",
+            "..#.#...#.#\n",
+            ".#...##..#.\n",
+            "..#.##.....\n",
+            ".#.#.#....#\n",
+            ".#........#\n",
+            "#.##...#...\n",
+            "#...##....#\n",
+            ".#..#...#.#\n",
+        ));
+        assert_eq!(solve(input), 7);
+    }
+}
