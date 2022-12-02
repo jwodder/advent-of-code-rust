fa90bcd7ee194e6f3a3d001df80c2bc910568764
John T. Wodder II <git@varonathe.org>
2022-12-02 18:54:23 -0500

    Solved 2020-09a!

---
+++ 2020/09a/src/main.rs
@@ -0,0 +1,48 @@
+use adventutil::Input;
+use std::collections::VecDeque;
+
+fn solve(input: Input, preamble_size: usize) -> u64 {
+    let mut iter = input.parse_lines::<u64>();
+    let mut preceding = VecDeque::with_capacity(preamble_size + 1);
+    for _ in 0..preamble_size {
+        preceding.push_back(iter.next().expect("Input lacks complete preamble"));
+    }
+    for n in iter {
+        if !is_sum(&preceding, n) {
+            return n;
+        }
+        preceding.push_back(n);
+        preceding.pop_front();
+    }
+    panic!("No solution found");
+}
+
+fn is_sum(numbers: &VecDeque<u64>, n: u64) -> bool {
+    for i in 0..numbers.len() {
+        for j in (i + 1)..numbers.len() {
+            if numbers[i] != numbers[j] && numbers[i] + numbers[j] == n {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env(), 25));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from(concat!(
+            "35\n", "20\n", "15\n", "25\n", "47\n", "40\n", "62\n", "55\n", "65\n", "95\n",
+            "102\n", "117\n", "150\n", "182\n", "127\n", "219\n", "299\n", "277\n", "309\n",
+            "576\n",
+        ));
+        assert_eq!(solve(input, 5), 127);
+    }
+}
