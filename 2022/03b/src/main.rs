98cb60648bf31004fc745f63a740afb4c86c2fe8
John T. Wodder II <git@varonathe.org>
2022-12-03 00:41:14 -0500

    Solved 2022-03b!

---
+++ 2022/03b/src/main.rs
@@ -0,0 +1,54 @@
+use adventutil::Input;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+fn solve(input: Input) -> u32 {
+    input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|mut w| {
+            let rs1 = w.next().unwrap().chars().collect::<HashSet<_>>();
+            let rs2 = w.next().unwrap().chars().collect::<HashSet<_>>();
+            let rs3 = w.next().unwrap().chars().collect::<HashSet<_>>();
+            priority(
+                rs1.intersection(&rs2.intersection(&rs3).copied().collect())
+                    .next()
+                    .copied()
+                    .unwrap(),
+            )
+        })
+        .sum()
+}
+
+fn priority(c: char) -> u32 {
+    if c.is_ascii_lowercase() {
+        (c as u32) - ('a' as u32) + 1
+    } else if c.is_ascii_uppercase() {
+        (c as u32) - ('A' as u32) + 27
+    } else {
+        panic!("Invalid character: {c:?}")
+    }
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from(concat!(
+            "vJrwpWtwJgWrhcsFMMfFFhFp\n",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n",
+            "PmmdzqPrVvPwwTWBwg\n",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n",
+            "ttgJtRGJQctTZtZT\n",
+            "CrZsJsPPZsGzwwsLwLmpwMDw\n",
+        ));
+        assert_eq!(solve(input), 70);
+    }
+}
