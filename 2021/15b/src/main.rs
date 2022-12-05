21694f35266606c0c2abdda09365879ae2dfdf99
John T. Wodder II <git@varonathe.org>
2022-12-04 20:47:54 -0500

    2021-15b: Runs for more than a minute without terminating

---
+++ 2021/15b/src/main.rs
@@ -0,0 +1,55 @@
+use adventutil::grid::{Coords, Direction, Grid, GridBounds};
+use adventutil::{Input, dijkstra_length};
+
+fn solve(input: Input) -> u32 {
+    let tile = input.parse::<Grid<u32>>();
+    let theight = tile.height();
+    let twidth = tile.width();
+    let bounds = GridBounds::new(tile.height() * 5, tile.width() * 5);
+    let grid = Grid::<u32>::from_fn(bounds, |(y, x)| {
+        let tx = u32::try_from(x / twidth).unwrap();
+        let x = x % twidth;
+        let ty = u32::try_from(y / theight).unwrap();
+        let y = y % theight;
+        let risk = (tile[(y, x)] + tx + ty) % 9;
+        if risk == 0 { 9 } else { risk }
+    });
+    let start = Coords::new(0, 0);
+    let end = Coords::new(grid.height() - 1, grid.width() - 1);
+    dijkstra_length(
+        start,
+        |&n| n == end,
+        |&coords| {
+            let cell = grid.get_cell(coords).unwrap();
+            Direction::cardinals()
+                .filter_map(move |d| cell.neighbor(d).map(|c| (c.coords(), *c.get())))
+        },
+    )
+    .expect("No route to end")
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from(concat!(
+            "1163751742\n",
+            "1381373672\n",
+            "2136511328\n",
+            "3694931569\n",
+            "7463417111\n",
+            "1319128137\n",
+            "1359912421\n",
+            "3125421639\n",
+            "1293138521\n",
+            "2311944581\n",
+        ));
+        assert_eq!(solve(input), 315);
+    }
+}
