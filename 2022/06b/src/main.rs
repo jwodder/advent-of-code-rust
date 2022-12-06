c511aeb8908c0442cfbe33007d565de3a1891d42
John T. Wodder II <git@varonathe.org>
2022-12-06 09:53:48 -0500

    Solved 2022-06b!

---
+++ 2022/06b/src/main.rs
@@ -0,0 +1,32 @@
+use adventutil::Input;
+use std::collections::HashSet;
+
+fn solve(input: Input) -> usize {
+    let chars = input.read().trim().chars().collect::<Vec<_>>();
+    for (i, w) in (14..).zip(chars.windows(14)) {
+        if w.iter().copied().collect::<HashSet<_>>().len() == 14 {
+            return i;
+        }
+    }
+    panic!("Start of packet not found");
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    #[case("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    #[case("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    fn test_solve(#[case] s: &'static str, #[case] output: usize) {
+        assert_eq!(solve(Input::from(s)), output);
+    }
+}
