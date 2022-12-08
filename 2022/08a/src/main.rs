ba8c17786ca8d1c454fdb4115ce2097a147d60f3
John T. Wodder II <git@varonathe.org>
2022-12-08 15:51:27 -0500

    Solved 2022-08a!

---
+++ 2022/08a/src/main.rs
@@ -0,0 +1,32 @@
+use adventutil::grid::{Direction, Grid};
+use adventutil::Input;
+use std::iter::successors;
+
+fn solve(input: Input) -> usize {
+    let grid = input.parse::<Grid<u32>>();
+    grid.iter_cells()
+        .filter(|&cell| {
+            let &height = cell.get();
+            Direction::cardinals().any(|d| {
+                successors(Some(cell), |c| c.neighbor(d))
+                    .skip(1)
+                    .all(|c| *c.get() < height)
+            })
+        })
+        .count()
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from("30373\n25512\n65332\n33549\n35390\n");
+        assert_eq!(solve(input), 21);
+    }
+}
