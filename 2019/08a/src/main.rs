3a3ea0408b098b3e3a8cc9f486f9ce375297d286
John T. Wodder II <git@varonathe.org>
2022-12-08 17:10:41 -0500

    Solved 2019-08a!

---
+++ 2019/08a/src/main.rs
@@ -0,0 +1,24 @@
+use adventutil::counter::Counter;
+use adventutil::Input;
+
+fn solve(input: Input) -> u64 {
+    let digits = input
+        .read()
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+    digits
+        .chunks(25 * 6)
+        .map(|layer| {
+            let counts = layer.iter().copied().collect::<Counter<_>>();
+            (counts[&0], counts[&1] * counts[&2])
+        })
+        .min_by_key(|&(qty0, _)| qty0)
+        .unwrap()
+        .1
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
