3c93f465d206bf0e86ef428f154e03b1fcd8c84c
John T. Wodder II <git@varonathe.org>
2022-12-10 16:06:11 -0500

    2015-19b: Try going in reverse; still doesn't terminate

--- 2015/19b/src/main.rs
+++ 2015/19b/src/main.rs
@@ -1,89 +1,50 @@
 use adventutil::pullparser::{ParseError, PullParser, Token};
-use adventutil::Input;
+use adventutil::{dijkstra_length, Input};
 use itertools::Itertools;
-use std::collections::{hash_map::Entry, HashMap, HashSet};
+use std::collections::HashMap;
 
-fn solve(input: Input) -> usize {
-    let mut paras = input.paragraphs();
-    let rules = paras
-        .next()
-        .expect("Empty input")
+fn solve(input: Input) -> u32 {
+    let (rules, molecule) = input
+        .paragraphs()
+        .collect_tuple()
+        .expect("Input was not exactly two paragraphs");
+    let rules = rules
         .lines()
-        .map(|s| parse_replacement(s).expect("Parse error"))
-        .into_group_map();
-    let molecule = split_atoms(paras.next().expect("No molecule in input"));
-    let mut current = vec!["e".to_string()];
-    let mut visited = HashSet::new();
-    let mut distances = HashMap::from([(current.clone(), 0usize)]);
-    loop {
-        //eprintln!("current = {current:?}");
-        for p in apply_replacements(&current, &rules) {
-            //eprintln!("  p = {p:?}");
-            if p.len() >= molecule.len() && p != molecule {
-                //eprintln!("  Discarded");
-                continue;
-            }
-            if !visited.contains(&p) {
-                let newdist = distances[&current] + 1;
-                //eprintln!("  newdist = {newdist}");
-                match distances.entry(p.clone()) {
-                    Entry::Vacant(e) => {
-                        e.insert(newdist);
-                    }
-                    Entry::Occupied(mut e) if *e.get() > newdist => {
-                        e.insert(newdist);
-                    }
-                    _ => (),
-                };
-            }
-        }
-        visited.insert(current);
-        if visited.contains(&molecule) {
-            return distances[&molecule];
-        }
-        current = distances
-            .iter()
-            .filter(|&(k, _)| !visited.contains(k))
-            .min_by_key(|&(_, &dist)| dist)
-            .map(|(k, _)| k.clone())
-            .expect("No route to endpoint");
-    }
-}
-
-fn apply_replacements<'a>(
-    molecule: &'a [String],
-    rules: &'a HashMap<String, Vec<Vec<String>>>,
-) -> impl Iterator<Item = Vec<String>> + 'a {
-    molecule
-        .iter()
-        .enumerate()
-        .filter_map(move |(i, atom)| {
-            Some(rules.get(atom)?.iter().map(move |r| {
-                let mut newmol = molecule.to_vec();
-                let _ = newmol.splice(i..=i, r.clone());
-                newmol
-            }))
+        .map(|s| {
+            let (before, after) = parse_replacement(s).expect("Parse error");
+            (after, before)
         })
-        .flatten()
+        .collect::<HashMap<String, String>>();
+    dijkstra_length(molecule, "e".to_string(), |current| {
+        apply_replacements(current, &rules)
+    })
+    .unwrap()
 }
 
-fn parse_replacement(s: &str) -> Result<(String, Vec<String>), ParseError> {
+fn apply_replacements(
+    molecule: &str,
+    rules: &HashMap<String, String>,
+) -> impl Iterator<Item = (String, u32)> {
+    let mut modified = Vec::new();
+    for (after, before) in rules {
+        let mut i = 0;
+        while let Some(n) = molecule[i..].find(after) {
+            let mut s = molecule[..n].to_string();
+            s.push_str(before);
+            s.push_str(&molecule[(n + after.len())..]);
+            modified.push(s);
+            i += n + 1;
+        }
+    }
+    modified.into_iter().map(|s| (s, 1))
+}
+
+fn parse_replacement(s: &str) -> Result<(String, String), ParseError> {
     let mut parser = PullParser::new(s);
     let left = parser.parse_to::<String, _>(Token::Whitespace)?;
     parser.skip("=> ")?;
     let right = parser.parse_to::<String, _>(Token::Eof)?;
-    Ok((left, split_atoms(right)))
-}
-
-fn split_atoms(s: String) -> Vec<String> {
-    s.chars()
-        .peekable()
-        .batching(|iter| {
-            let mut atom = String::from(iter.next()?);
-            atom.extend(iter.take_while_ref(|c| c.is_ascii_lowercase()));
-            Some(atom)
-        })
-        .collect()
+    Ok((left, right))
 }
 
 fn main() {
@@ -93,14 +54,6 @@ fn main() {
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn test_split_atoms() {
-        assert_eq!(
-            split_atoms("CRnFYMgAr".into()),
-            ["C", "Rn", "F", "Y", "Mg", "Ar"]
-        );
-    }
 
     #[test]
     fn test_example1() {
