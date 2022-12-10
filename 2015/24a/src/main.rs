769ce19680a65ab682e73055937aed9d6db8f425
John T. Wodder II <git@varonathe.org>
2022-12-09 20:39:48 -0500

    Solved 2015-24a!

---
+++ 2015/24a/src/main.rs
@@ -0,0 +1,100 @@
+use adventutil::Input;
+use itertools::Itertools;
+
+struct Partition {
+    // The weights of the packages in the passenger compartment, in ascending
+    // order
+    passenger: Vec<u64>,
+    // The weights of the rest of the packages, in ascending order
+    remainder: Vec<u64>,
+}
+
+impl Partition {
+    fn is_valid(&self, target_weight: u64) -> bool {
+        self.passenger.iter().copied().sum::<u64>() == target_weight
+            && partitionable(&self.remainder, target_weight)
+    }
+
+    fn next_rank(self) -> impl Iterator<Item = Partition> {
+        let i = self
+            .remainder
+            .binary_search(self.passenger.last().unwrap())
+            .unwrap_err();
+        (i..(self.remainder.len())).map(move |j| {
+            let mut passenger = self.passenger.clone();
+            let mut remainder = self.remainder.clone();
+            passenger.push(remainder.remove(j));
+            Partition {
+                passenger,
+                remainder,
+            }
+        })
+    }
+
+    fn entanglement(&self) -> u64 {
+        self.passenger.iter().copied().product()
+    }
+}
+
+fn solve(input: Input) -> u64 {
+    // The input weights are unique and in sorted/ascending order.
+    let weights = input.parse_lines::<u64>().collect::<Vec<_>>();
+    let target_weight = weights.iter().copied().sum::<u64>() / 3;
+    let mut partitions = (0..(weights.len()))
+        .map(|i| {
+            let mut weights = weights.clone();
+            let x = weights.remove(i);
+            Partition {
+                passenger: vec![x],
+                remainder: weights,
+            }
+        })
+        .collect::<Vec<_>>();
+    while !partitions.is_empty() {
+        let valid = partitions
+            .iter()
+            .filter(|p| p.is_valid(target_weight))
+            .collect::<Vec<_>>();
+        if !valid.is_empty() {
+            return valid
+                .into_iter()
+                .map(Partition::entanglement)
+                .min()
+                .unwrap();
+        }
+        partitions = partitions
+            .into_iter()
+            .flat_map(Partition::next_rank)
+            .collect::<Vec<_>>();
+    }
+    panic!("No solution found");
+}
+
+/// Tests whether the values in `values` can be partitioned into two groups
+/// that each sum up to `target_weight`.
+///
+/// # Precondition (Not Checked)
+///
+/// The sum of the elements in `values` must be twice `target_weight`.
+fn partitionable(values: &[u64], target_weight: u64) -> bool {
+    values
+        .iter()
+        .copied()
+        .powerset()
+        .any(|s| s.into_iter().sum::<u64>() == target_weight)
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from("1\n2\n3\n4\n5\n7\n8\n9\n10\n11\n");
+        assert_eq!(solve(input), 99);
+    }
+}
