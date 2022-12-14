019b9b27e732d8c6e9953b0d16bb16f0863056c2
John T. Wodder II <git@varonathe.org>
2022-12-14 18:39:49 -0500

    Solved 2016-06a!

---
+++ 2016/06a/src/main.rs
@@ -0,0 +1,35 @@
+use adventutil::counter::Counter;
+use adventutil::Input;
+
+fn solve(input: Input) -> String {
+    let input = input
+        .lines()
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let mut output = String::new();
+    for i in 0..(input[0].len()) {
+        let counts = input.iter().map(|ln| ln[i]).collect::<Counter<_>>();
+        let (c, _) = counts.into_iter().max_by_key(|&(_, qty)| qty).unwrap();
+        output.push(c);
+    }
+    output
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let input = Input::from(concat!(
+            "eedadn\n", "drvtee\n", "eandsr\n", "raavrd\n", "atevrs\n", "tsrnev\n", "sdttsa\n",
+            "rasrtv\n", "nssdts\n", "ntnada\n", "svetve\n", "tesnvt\n", "vntsnd\n", "vrdear\n",
+            "dvrsen\n", "enarar\n",
+        ));
+        assert_eq!(solve(input), "easter");
+    }
+}
