0ecb2fd32127b76f4eec72005f8971349815180d
John T. Wodder II <git@varonathe.org>
2025-01-31 18:17:19 -0500

    Solved 2016-10a

---
+++ 2016/10a/src/main.rs
@@ -0,0 +1,109 @@
+use adventutil::pullparser::{ParseError, PullParser, Token};
+use adventutil::Input;
+use std::collections::{HashMap, VecDeque};
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Give {
+    chip: u32,
+    bot: u32,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Split {
+    bot: u32,
+    low_target: Destination,
+    high_target: Destination,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Destination {
+    Output(u32),
+    Bot(u32),
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Instruction {
+    Give(Give),
+    Split(Split),
+}
+
+impl std::str::FromStr for Instruction {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Instruction, ParseError> {
+        let mut parser = PullParser::new(s);
+        if parser.skip("value ").is_ok() {
+            let chip = parser.parse_to::<u32, _>(Token::Whitespace)?;
+            parser.skip("goes to bot ")?;
+            let bot = parser.parse_to::<u32, _>(Token::Eof)?;
+            Ok(Instruction::Give(Give { chip, bot }))
+        } else {
+            parser.skip("bot ")?;
+            let bot = parser.parse_to::<u32, _>(Token::Whitespace)?;
+            parser.skip("gives low to ")?;
+            let low_target = if parser.skip("bot ").is_ok() {
+                Destination::Bot(parser.parse_to::<u32, _>(Token::Whitespace)?)
+            } else {
+                parser.skip("output ")?;
+                Destination::Output(parser.parse_to::<u32, _>(Token::Whitespace)?)
+            };
+            parser.skip("and high to ")?;
+            let high_target = if parser.skip("bot ").is_ok() {
+                Destination::Bot(parser.parse_to::<u32, _>(Token::Eof)?)
+            } else {
+                parser.skip("output ")?;
+                Destination::Output(parser.parse_to::<u32, _>(Token::Eof)?)
+            };
+            Ok(Instruction::Split(Split {
+                bot,
+                low_target,
+                high_target,
+            }))
+        }
+    }
+}
+
+fn solve(input: Input) -> u32 {
+    let mut holding = HashMap::<u32, Vec<u32>>::new();
+    let mut splits = HashMap::new();
+    let mut givings = VecDeque::new();
+    for instr in input.parse_lines::<Instruction>() {
+        match instr {
+            Instruction::Give(g) => givings.push_back(g),
+            Instruction::Split(sp) => {
+                splits.insert(sp.bot, sp);
+            }
+        }
+    }
+    while let Some(g) = givings.pop_front() {
+        let held = holding.entry(g.bot).or_default();
+        held.push(g.chip);
+        if held.len() == 2 {
+            let x = held.pop().unwrap();
+            let y = held.pop().unwrap();
+            let high_chip = x.max(y);
+            let low_chip = x.min(y);
+            if (low_chip, high_chip) == (17, 61) {
+                return g.bot;
+            }
+            let sp = splits[&g.bot];
+            if let Destination::Bot(b) = sp.low_target {
+                givings.push_back(Give {
+                    chip: low_chip,
+                    bot: b,
+                });
+            }
+            if let Destination::Bot(b) = sp.high_target {
+                givings.push_back(Give {
+                    chip: high_chip,
+                    bot: b,
+                });
+            }
+        }
+    }
+    panic!("Target comparison not found");
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
