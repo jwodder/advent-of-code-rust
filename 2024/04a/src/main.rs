62a9b5e99cf7e9402a914efaee53bfc8ff5e857d
John T. Wodder II <git@varonathe.org>
2025-02-01 17:35:23 -0500

    Solved 2024-04a

---
+++ 2024/04a/src/main.rs
@@ -0,0 +1,59 @@
+use adventutil::grid::{Direction, Grid};
+use adventutil::Input;
+
+fn solve(input: Input) -> u32 {
+    let grid = input.parse::<Grid<char>>();
+    let mut qty = 0;
+    for cell in grid.iter_cells() {
+        if *cell == 'X' {
+            for d in Direction::adjacent() {
+                let Some(c2) = cell.neighbor(d) else {
+                    continue;
+                };
+                if *c2 != 'M' {
+                    continue;
+                }
+                let Some(c3) = c2.neighbor(d) else {
+                    continue;
+                };
+                if *c3 != 'A' {
+                    continue;
+                }
+                let Some(c4) = c3.neighbor(d) else {
+                    continue;
+                };
+                if *c4 != 'S' {
+                    continue;
+                }
+                qty += 1;
+            }
+        }
+    }
+    qty
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = Input::from(concat!(
+            "MMMSXXMASM\n",
+            "MSAMXMSMSA\n",
+            "AMXSXMAAMM\n",
+            "MSAMASMSMX\n",
+            "XMASAMXAMM\n",
+            "XXAMMXXAMA\n",
+            "SMSMSASXSS\n",
+            "SAXAMASAAA\n",
+            "MAMMMXMMMM\n",
+            "MXMXAXMASX\n",
+        ));
+        assert_eq!(solve(input), 18);
+    }
+}
