6975914117212d35ba795e13a02e03b2c9a9ef76
John T. Wodder II <git@varonathe.org>
2025-02-01 16:32:16 -0500

    Solved 2024-02a

---
+++ 2024/02a/src/main.rs
@@ -0,0 +1,59 @@
+use adventutil::Input;
+
+fn is_gradual<I: IntoIterator<Item = u32>>(report: I) -> bool {
+    let mut iter = report.into_iter();
+    let Some(prev) = iter.next() else {
+        return true;
+    };
+    let Some(n) = iter.next() else {
+        return true;
+    };
+    let ascending = n > prev;
+    if !(1..=3).contains(&n.abs_diff(prev)) {
+        return false;
+    }
+    let mut prev = n;
+    for n in iter {
+        if (ascending && n <= prev) || (!ascending && n >= prev) {
+            return false;
+        }
+        if !(1..=3).contains(&n.abs_diff(prev)) {
+            return false;
+        }
+        prev = n;
+    }
+    true
+}
+
+fn solve(input: Input) -> usize {
+    input
+        .lines()
+        .filter(|ln| {
+            is_gradual(
+                ln.split_ascii_whitespace()
+                    .map(|s| s.parse::<u32>().unwrap()),
+            )
+        })
+        .count()
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case([7, 6, 4, 2, 1], true)]
+    #[case([1, 2, 7, 8, 9], false)]
+    #[case([9, 7, 6, 2, 1], false)]
+    #[case([1, 3, 2, 4, 5], false)]
+    #[case([8, 6, 4, 4, 1], false)]
+    #[case([1, 3, 6, 7, 9], true)]
+    fn test_is_gradual(#[case] report: [u32; 5], #[case] r: bool) {
+        assert_eq!(is_gradual(report), r);
+    }
+}
