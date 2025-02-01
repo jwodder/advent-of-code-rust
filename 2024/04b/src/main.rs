5c1ac09a05200671944ebf14ab4910d7db948f5d
John T. Wodder II <git@varonathe.org>
2025-02-01 17:43:08 -0500

    Solved 2024-04b

---
+++ 2024/04b/src/main.rs
@@ -0,0 +1,48 @@
+use adventutil::grid::{Direction, Grid};
+use adventutil::Input;
+
+fn solve(input: Input) -> usize {
+    let grid = input.parse::<Grid<char>>();
+    grid.iter_cells()
+        .filter(|cell| {
+            *cell == 'A'
+                && [Direction::NorthEast, Direction::NorthWest]
+                    .into_iter()
+                    .all(|d| {
+                        let Some(c2) = cell.neighbor(d) else {
+                            return false;
+                        };
+                        let Some(c3) = cell.neighbor(-d) else {
+                            return false;
+                        };
+                        (*c2 == 'M' && *c3 == 'S') || (*c2 == 'S' && *c3 == 'M')
+                    })
+        })
+        .count()
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = Input::from(concat!(
+            "MMMSXXMASM\n",
+            "MSAMXMSMSA\n",
+            "AMXSXMAAMM\n",
+            "MSAMASMSMX\n",
+            "XMASAMXAMM\n",
+            "XXAMMXXAMA\n",
+            "SMSMSASXSS\n",
+            "SAXAMASAAA\n",
+            "MAMMMXMMMM\n",
+            "MXMXAXMASX\n",
+        ));
+        assert_eq!(solve(input), 9);
+    }
+}
