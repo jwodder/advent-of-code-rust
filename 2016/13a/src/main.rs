532beb5a78833ad8413b783b5db30fb86ce2f9cd
John T. Wodder II <git@varonathe.org>
2025-01-31 19:02:36 -0500

    Solved 2016-13a

---
+++ 2016/13a/src/main.rs
@@ -0,0 +1,36 @@
+use adventutil::{dijkstra_length, Input};
+
+fn is_open(key: i32, x: i32, y: i32) -> bool {
+    let value = x * x + 3 * x + 2 * x * y + y + y * y + key;
+    value.count_ones() % 2 == 0
+}
+
+fn steps(key: i32, target_x: i32, target_y: i32) -> u32 {
+    dijkstra_length((1, 1), (target_x, target_y), |&(x, y)| {
+        [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            .into_iter()
+            .map(move |(i, j)| (x + i, y + j))
+            .filter(|&(i, j)| is_open(key, i, j))
+            .map(|p| (p, 1))
+    })
+    .unwrap()
+}
+
+fn solve(input: Input) -> u32 {
+    let key = input.parse::<i32>();
+    steps(key, 31, 39)
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert_eq!(steps(10, 7, 4), 11);
+    }
+}
