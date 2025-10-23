d4a28c2bcbb0e074e4facd5560fa4eeb17ae9d85
John T. Wodder II <git@varonathe.org>
2025-10-23 15:39:50 -0400

    Solved 2017-19a

---
+++ 2017/19a/src/main.rs
@@ -0,0 +1,48 @@
+use adventutil::grid::{Coords, Direction, Grid};
+use adventutil::Input;
+
+fn solve(input: Input) -> String {
+    // We can't use input.parse() here, as that trims the input.
+    let map = input.read().parse::<Grid<char>>().unwrap();
+    let start_x = (0..(map.width())).position(|x| map[(0, x)] == '|').unwrap();
+    let mut cell = map.get_cell(Coords { y: 0, x: start_x }).unwrap();
+    let mut d = Direction::South;
+    let mut letters = String::new();
+    loop {
+        match *cell {
+            '|' | '-' => (),
+            c if c.is_ascii_alphabetic() => letters.push(c),
+            '+' => {
+                d = Direction::cardinals()
+                    .find(|&d2| d2 != -d && cell.neighbor(d2).as_deref() != Some(&' '))
+                    .unwrap();
+            }
+            ' ' => break,
+            c => panic!("Unexpected tile {c:?}"),
+        }
+        cell = cell.neighbor(d).unwrap();
+    }
+    letters
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let input = Input::from(concat!(
+            "     |          \n",
+            "     |  +--+    \n",
+            "     A  |  C    \n",
+            " F---|----E|--+ \n",
+            "     |  |  |  D \n",
+            "     +B-+  +--+ \n",
+        ));
+        assert_eq!(solve(input), "ABCDEF");
+    }
+}
