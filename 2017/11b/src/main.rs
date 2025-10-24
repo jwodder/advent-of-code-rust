aecdb3ea90a2aee167e593dacd01027bc9fccf8e
John T. Wodder II <git@varonathe.org>
2025-10-24 13:29:52 -0400

    Solved 2017-11b

---
+++ 2017/11b/src/main.rs
@@ -0,0 +1,96 @@
+use adventutil::maxtracker::MaxTracker;
+use adventutil::Input;
+use thiserror::Error;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct HexCoord {
+    // On rows with odd `y` values, only cells with odd `x` values exist, and
+    // likewise for evens.
+    x: i32,
+    y: i32,
+}
+
+impl HexCoord {
+    fn step(self, st: Step) -> HexCoord {
+        match st {
+            Step::North => HexCoord {
+                x: self.x,
+                y: self.y + 2,
+            },
+            Step::NorthEast => HexCoord {
+                x: self.x + 1,
+                y: self.y + 1,
+            },
+            Step::SouthEast => HexCoord {
+                x: self.x + 1,
+                y: self.y - 1,
+            },
+            Step::South => HexCoord {
+                x: self.x,
+                y: self.y - 2,
+            },
+            Step::SouthWest => HexCoord {
+                x: self.x - 1,
+                y: self.y - 1,
+            },
+            Step::NorthWest => HexCoord {
+                x: self.x - 1,
+                y: self.y + 1,
+            },
+        }
+    }
+
+    fn distance_from_origin(self) -> i32 {
+        let mut y = self.y.abs();
+        let mut x = self.x.abs();
+        let diag_steps = std::cmp::min(x, y);
+        y -= diag_steps;
+        x -= diag_steps;
+        let vert_steps = y / 2 + x;
+        diag_steps + vert_steps
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Step {
+    North,
+    NorthEast,
+    SouthEast,
+    South,
+    SouthWest,
+    NorthWest,
+}
+
+impl std::str::FromStr for Step {
+    type Err = ParseStepError;
+
+    fn from_str(s: &str) -> Result<Step, ParseStepError> {
+        match s {
+            "n" => Ok(Step::North),
+            "ne" => Ok(Step::NorthEast),
+            "se" => Ok(Step::SouthEast),
+            "s" => Ok(Step::South),
+            "sw" => Ok(Step::SouthWest),
+            "nw" => Ok(Step::NorthWest),
+            _ => Err(ParseStepError(s.to_owned())),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, Error, PartialEq)]
+#[error("invalid step: {0:?}")]
+struct ParseStepError(String);
+
+fn solve(input: Input) -> i32 {
+    let mut loc = HexCoord { x: 0, y: 0 };
+    let mut tracker = MaxTracker::new();
+    for st in input.parse_csv_line::<Step>() {
+        loc = loc.step(st);
+        tracker.add(loc.distance_from_origin());
+    }
+    tracker.get().unwrap()
+}
+
+fn main() {
+    println!("{}", solve(Input::from_env()));
+}
