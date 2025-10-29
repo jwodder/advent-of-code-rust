c7f3b1568d28143e5d8e5f0a16217284bb79158d
John T. Wodder II <git@varonathe.org>
2025-10-29 13:03:30 -0400

    2018-15a: Use Rc<RefCell<Unit>> instead of indexing

--- 2018/15a/src/main.rs
+++ 2018/15a/src/main.rs
@@ -1,10 +1,12 @@
 use adventutil::grid::{Coords, Direction, Grid, GridBounds, ParseGridError};
 use adventutil::Input;
+use std::cell::RefCell;
 use std::collections::{BTreeMap, HashMap, HashSet};
+use std::rc::Rc;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct Battlefield {
-    units: HashMap<usize, Unit>,
+    units: Vec<Rc<RefCell<Unit>>>,
     bounds: GridBounds,
     open: HashSet<Coords>,
 }
@@ -12,15 +14,11 @@ struct Battlefield {
 impl Battlefield {
     // Returns `true` if the round ended with one side's victory
     fn round(&mut self) -> bool {
-        let mut unit_order = self
-            .units
-            .iter()
-            .map(|(&i, unit)| (i, unit.clone()))
-            .collect::<Vec<_>>();
-        unit_order.sort_unstable_by_key(|p| p.1.pos());
-        for (id, mut unit) in unit_order {
-            if self.units.contains_key(&id) {
-                unit = self.move_unit(id, unit);
+        let mut unit_order = self.units.clone();
+        unit_order.sort_unstable_by_key(|u| u.borrow().pos());
+        for unit in unit_order {
+            if unit.borrow().health() > 0 {
+                self.move_unit(Rc::clone(&unit));
                 if !self.attack(unit) {
                     return true;
                 }
@@ -30,16 +28,16 @@ impl Battlefield {
     }
 
     fn remaining_hp(&self) -> u32 {
-        self.units.values().map(Unit::health).sum()
+        self.units.iter().map(|u| u.borrow().health()).sum()
     }
 
-    fn move_unit(&mut self, unit_id: usize, mut unit: Unit) -> Unit {
-        let enemy = unit.species.enemy();
+    fn move_unit(&mut self, unit: Rc<RefCell<Unit>>) {
+        let enemy = unit.borrow().species.enemy();
         let mut in_range = HashSet::new();
-        for u in self.units.values() {
-            if u.species() == enemy {
+        for u in self.units.clone() {
+            if u.borrow().species() == enemy {
                 for d in Direction::cardinals() {
-                    if let Some(c) = self.bounds.move_in(u.pos(), d) {
+                    if let Some(c) = self.bounds.move_in(u.borrow().pos(), d) {
                         if self.open.contains(&c) {
                             in_range.insert(c);
                         }
@@ -47,12 +45,13 @@ impl Battlefield {
                 }
             }
         }
-        if in_range.is_empty() || in_range.contains(&unit.pos()) {
-            return unit;
+        let start = unit.borrow().pos();
+        if in_range.is_empty() || in_range.contains(&start) {
+            return;
         }
 
         let mut visited = HashSet::new();
-        let mut pos2paths = HashMap::from([(unit.pos(), vec![Vec::new()])]);
+        let mut pos2paths = HashMap::from([(start, vec![Vec::new()])]);
         let mut shortest = BTreeMap::<Coords, Vec<Vec<Coords>>>::new();
         while shortest.is_empty() {
             let mut pos2paths2 = HashMap::<Coords, Vec<Vec<Coords>>>::new();
@@ -76,50 +75,46 @@ impl Battlefield {
             pos2paths = pos2paths2;
             if pos2paths.is_empty() {
                 // No paths to any in_range coords
-                return unit;
+                return;
             }
         }
         let (_, paths) = shortest.pop_first().unwrap();
         let next_pos = paths.into_iter().map(|pth| pth[0]).min().unwrap();
 
-        self.open.insert(unit.pos);
+        self.open.insert(start);
         self.open.remove(&next_pos);
-        unit.pos = next_pos;
-        self.units.insert(unit_id, unit.clone());
-        unit
+        unit.borrow_mut().pos = next_pos;
     }
 
     // Returns `false` if there were no enemies anywhere on the map to attack
-    fn attack(&mut self, unit: Unit) -> bool {
+    fn attack(&mut self, unit: Rc<RefCell<Unit>>) -> bool {
         let adjacent_coords = Direction::cardinals()
-            .filter_map(|d| self.bounds.move_in(unit.pos, d))
+            .filter_map(|d| self.bounds.move_in(unit.borrow().pos, d))
             .collect::<HashSet<_>>();
-        let enemy = unit.species.enemy();
+        let enemy = unit.borrow().species.enemy();
         let mut adj_enemies = Vec::new();
         let mut any_enemies = false;
-        for (&id, u) in &self.units {
-            if u.species() == enemy {
+        for u in self.units.clone() {
+            if u.borrow().species() == enemy {
                 any_enemies = true;
-                if adjacent_coords.contains(&u.pos()) {
-                    adj_enemies.push((id, u.clone()));
+                if adjacent_coords.contains(&u.borrow().pos()) {
+                    adj_enemies.push(u);
                 }
             }
         }
         if !any_enemies {
             return false;
         }
-        if let Some(target_hp) = adj_enemies.iter().map(|(_, u)| u.health()).min() {
-            let (target_id, mut target) = adj_enemies
+        if let Some(target_hp) = adj_enemies.iter().map(|u| u.borrow().health()).min() {
+            let target = adj_enemies
                 .into_iter()
-                .filter(|p| p.1.health() == target_hp)
-                .min_by_key(|p| p.1.pos())
+                .filter(|u| u.borrow().health() == target_hp)
+                .min_by_key(|u| u.borrow().pos())
                 .unwrap();
-            target.health = target.health.saturating_sub(3);
-            if target.health == 0 {
-                self.units.remove(&target_id);
-                self.open.insert(target.pos());
-            } else {
-                self.units.insert(target_id, target);
+            let mut t = target.borrow_mut();
+            t.health = t.health.saturating_sub(3);
+            if t.health == 0 {
+                self.open.insert(t.pos());
             }
         }
         true
@@ -131,8 +126,7 @@ impl std::str::FromStr for Battlefield {
 
     fn from_str(s: &str) -> Result<Battlefield, Self::Err> {
         let grid = s.parse::<Grid<char>>()?;
-        let mut i = 0;
-        let mut units = HashMap::new();
+        let mut units = Vec::new();
         let bounds = grid.bounds();
         let mut open = HashSet::new();
         for (pos, &ch) in &grid {
@@ -142,26 +136,18 @@ impl std::str::FromStr for Battlefield {
                 }
                 '#' => (),
                 'G' => {
-                    units.insert(
-                        i,
-                        Unit {
-                            pos,
-                            health: 200,
-                            species: Species::Goblin,
-                        },
-                    );
-                    i += 1;
+                    units.push(Rc::new(RefCell::new(Unit {
+                        pos,
+                        health: 200,
+                        species: Species::Goblin,
+                    })));
                 }
                 'E' => {
-                    units.insert(
-                        i,
-                        Unit {
-                            pos,
-                            health: 200,
-                            species: Species::Elf,
-                        },
-                    );
-                    i += 1;
+                    units.push(Rc::new(RefCell::new(Unit {
+                        pos,
+                        health: 200,
+                        species: Species::Elf,
+                    })));
                 }
                 _ => unreachable!(), // TODO: Use a real error
             }
