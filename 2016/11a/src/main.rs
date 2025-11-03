3ad6c81234d851b6b9d165e1d7f916ec754725af
John T. Wodder II <git@varonathe.org>
2025-11-03 14:43:34 -0500

    2016-11a: Some minor improvements

--- 2016/11a/src/main.rs
+++ 2016/11a/src/main.rs
@@ -58,6 +58,9 @@ impl State {
         if self.elevator == 0 {
             return None;
         }
+        if self.floors[0..self.elevator].iter().all(BTreeSet::is_empty) {
+            return None;
+        }
         let mut newstate = self.clone();
         for it in items {
             if !newstate.floors[newstate.elevator].remove(it) {
@@ -90,13 +93,29 @@ fn is_safe(set: &BTreeSet<Item>) -> bool {
     microchips.is_subset(&generators) || generators.is_empty()
 }
 
+/// Return all one-element and two-element subsets of `set`, with the following
+/// exceptions:
+///
+/// - If there are any pairs of a chip with its generator, return the first
+///   pair only; any further pairs produce isomorphic (and thus redundant)
+///   states.
 fn pick_ones_and_twos(set: &BTreeSet<Item>) -> Vec<Vec<&Item>> {
     let items = set.iter().collect::<Vec<_>>();
     let mut picked = Vec::new();
+    let mut got_pair = false;
     for i in 0..(items.len()) {
         picked.push(vec![items[i]]);
         for j in (i + 1)..(items.len()) {
-            picked.push(vec![items[i], items[j]]);
+            let it1 = items[i];
+            let it2 = items[j];
+            if it1.matches(it2) {
+                if !got_pair {
+                    picked.push(vec![it1, it2]);
+                    got_pair = true;
+                }
+            } else {
+                picked.push(vec![it1, it2]);
+            }
         }
     }
     picked
@@ -149,6 +168,16 @@ fn parse_item_list(s: &str) -> Result<BTreeSet<Item>, ParseError> {
 enum Item {
     Microchip(String),
     Generator(String),
+}
+
+impl Item {
+    fn matches(&self, other: &Item) -> bool {
+        match (self, other) {
+            (Item::Microchip(name1), Item::Generator(name2)) => name1 == name2,
+            (Item::Generator(name1), Item::Microchip(name2)) => name1 == name2,
+            _ => false,
+        }
+    }
 }
 
 impl std::str::FromStr for Item {
