93762db7031c5ecda5c6d7b58f73fb77bf3248a1
John T. Wodder II <git@varonathe.org>
2025-11-05 13:34:25 -0500

    Tiny speedup to 2016-11a

--- 2016/11a/src/main.rs
+++ 2016/11a/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::BTreeSet;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct State {
-    floors: [BTreeSet<Item>; 4],
+    floors: [Items; 4],
     elevator: usize,
 }
 
@@ -15,101 +15,138 @@ impl State {
 
     fn advancements(&self) -> Vec<State> {
         let mut next_states = Vec::new();
-        for picked in pick_ones_and_twos(&self.floors[self.elevator]) {
+        for picked in self.floors[self.elevator].pick_ones_and_twos() {
             next_states.extend(self.move_up_with(&picked));
             next_states.extend(self.move_down_with(&picked));
         }
         next_states
     }
 
-    fn move_up_with(&self, items: &[&Item]) -> Option<State> {
+    fn move_up_with(&self, items: &Items) -> Option<State> {
         if self.elevator >= 3 {
             return None;
         }
         let mut newstate = self.clone();
-        for it in items {
-            if !newstate.floors[newstate.elevator].remove(it) {
-                return None;
-            }
-        }
-        if !is_safe(&newstate.floors[newstate.elevator]) {
-            return None;
-        }
+        newstate.floors[newstate.elevator].remove(items);
+        assert!(newstate.floors[newstate.elevator].is_safe());
         newstate.elevator += 1;
-        for it in items {
-            newstate.floors[newstate.elevator].insert((*it).clone());
-        }
-        if !is_safe(&newstate.floors[newstate.elevator]) {
+        newstate.floors[newstate.elevator].extend(items.clone());
+        if !newstate.floors[newstate.elevator].is_safe() {
             return None;
         }
         Some(newstate)
     }
 
-    fn move_down_with(&self, items: &[&Item]) -> Option<State> {
+    fn move_down_with(&self, items: &Items) -> Option<State> {
         if self.elevator == 0 {
             return None;
         }
-        if self.floors[0..self.elevator].iter().all(BTreeSet::is_empty) {
+        if self.floors[0..self.elevator].iter().all(Items::is_empty) {
             return None;
         }
         let mut newstate = self.clone();
-        for it in items {
-            if !newstate.floors[newstate.elevator].remove(it) {
-                return None;
-            }
-        }
-        if !is_safe(&newstate.floors[newstate.elevator]) {
-            return None;
-        }
+        newstate.floors[newstate.elevator].remove(items);
+        assert!(newstate.floors[newstate.elevator].is_safe());
         newstate.elevator -= 1;
-        for it in items {
-            newstate.floors[newstate.elevator].insert((*it).clone());
-        }
-        if !is_safe(&newstate.floors[newstate.elevator]) {
+        newstate.floors[newstate.elevator].extend(items.clone());
+        if !newstate.floors[newstate.elevator].is_safe() {
             return None;
         }
         Some(newstate)
     }
 }
 
-fn is_safe(set: &BTreeSet<Item>) -> bool {
-    let mut microchips = BTreeSet::new();
-    let mut generators = BTreeSet::new();
-    for item in set {
-        match item {
-            Item::Microchip(element) => microchips.insert(element),
-            Item::Generator(element) => generators.insert(element),
-        };
-    }
-    microchips.is_subset(&generators) || generators.is_empty()
+#[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
+struct Items {
+    microchips: BTreeSet<String>,
+    generators: BTreeSet<String>,
 }
 
-/// Return all one-element and two-element subsets of `set`, with the following
-/// exceptions:
-///
-/// - If there are any pairs of a chip with its generator, return the first
-///   pair only; any further pairs produce isomorphic (and thus redundant)
-///   states.
-fn pick_ones_and_twos(set: &BTreeSet<Item>) -> Vec<Vec<&Item>> {
-    let items = set.iter().collect::<Vec<_>>();
-    let mut picked = Vec::new();
-    let mut got_pair = false;
-    for i in 0..(items.len()) {
-        picked.push(vec![items[i]]);
-        for j in (i + 1)..(items.len()) {
-            let it1 = items[i];
-            let it2 = items[j];
-            if it1.matches(it2) {
-                if !got_pair {
-                    picked.push(vec![it1, it2]);
-                    got_pair = true;
-                }
-            } else {
-                picked.push(vec![it1, it2]);
-            }
+impl Items {
+    fn is_empty(&self) -> bool {
+        self.microchips.is_empty() && self.generators.is_empty()
+    }
+
+    fn is_safe(&self) -> bool {
+        self.microchips.is_subset(&self.generators) || self.generators.is_empty()
+    }
+
+    fn remove(&mut self, other: &Items) {
+        for chip in &other.microchips {
+            self.microchips.remove(chip);
+        }
+        for generator in &other.generators {
+            self.generators.remove(generator);
         }
     }
-    picked
+
+    fn extend(&mut self, other: Items) {
+        self.microchips.extend(other.microchips);
+        self.generators.extend(other.generators);
+    }
+
+    /// Return all one-element and two-element subsets of `self`, with the
+    /// following exceptions:
+    ///
+    /// - For safety reasons, if `self` contains both a chip and its generator,
+    ///   and there are other generators, don't take the first generator
+    ///   without the chip; if you're taking the generator, also take either
+    ///   the chip or, if there is exactly one other generator, the other
+    ///   generator.
+    ///
+    /// - As an optimization, if `self` contains any pairs of a chip with its
+    ///   generator, return the first pair only; any further pairs produce
+    ///   isomorphic (and thus redundant) states.
+    ///
+    /// This simplifies to the following:
+    ///
+    /// - All one- & two-element sets of microchips
+    ///
+    /// - All one- & two-element sets of generators that do not have
+    ///   corresponding microchips
+    ///
+    /// - If there is only one generator, that generator (unless it lacks a
+    ///   corresponding microchip, in which case it would be a duplicate)
+    ///
+    /// - If there are only two generators, both generators (unless they both
+    ///   lack corresponding microchips, in which case this would be a
+    ///   duplicate)
+    ///
+    /// - Exactly one matching microchip-generator pair, if there are any such
+    ///   pairs
+    fn pick_ones_and_twos(&self) -> Vec<Items> {
+        let mut picked = Vec::new();
+        let chips = self.microchips.iter().collect::<Vec<_>>();
+        for microchips in pick_ones_and_twos(&chips) {
+            picked.push(Items {
+                microchips,
+                generators: BTreeSet::new(),
+            });
+        }
+        let unmatched_gens = self
+            .generators
+            .difference(&self.microchips)
+            .collect::<Vec<_>>();
+        for generators in pick_ones_and_twos(&unmatched_gens) {
+            picked.push(Items {
+                microchips: BTreeSet::new(),
+                generators,
+            });
+        }
+        if matches!(self.generators.len(), 1 | 2) && !self.generators.is_subset(&self.microchips) {
+            picked.push(Items {
+                microchips: BTreeSet::new(),
+                generators: self.generators.clone(),
+            });
+        }
+        if let Some(name) = self.microchips.intersection(&self.generators).next() {
+            picked.push(Items {
+                microchips: [name.clone()].into(),
+                generators: [name.clone()].into(),
+            });
+        }
+        picked
+    }
 }
 
 impl std::str::FromStr for State {
@@ -130,45 +167,51 @@ impl std::str::FromStr for State {
         //parser.skip(Token::Newline)?;
         parser.eof()?;
         Ok(State {
-            floors: [first, second, third, BTreeSet::new()],
+            floors: [first, second, third, Items::default()],
             elevator: 0,
         })
     }
 }
 
-fn parse_item_list(s: &str) -> Result<BTreeSet<Item>, ParseError> {
-    if s.contains(',') {
+fn parse_item_list(s: &str) -> Result<Items, ParseError> {
+    let unsorted = if s.contains(',') {
         s.split(',')
             .map(|t| {
                 let t = t.trim();
                 let t = t.strip_prefix("and ").unwrap_or(t);
                 t.parse::<Item>()
             })
-            .collect()
+            .collect::<Result<Vec<_>, _>>()?
     } else if let Some((pre, post)) = s.split_once(" and ") {
-        Ok(BTreeSet::from([
-            pre.parse::<Item>()?,
-            post.parse::<Item>()?,
-        ]))
+        vec![pre.parse::<Item>()?, post.parse::<Item>()?]
     } else {
-        Ok(BTreeSet::from([s.parse::<Item>()?]))
+        vec![s.parse::<Item>()?]
+    };
+    let mut items = Items::default();
+    for it in unsorted {
+        match it {
+            Item::Microchip(name) => items.microchips.insert(name),
+            Item::Generator(name) => items.generators.insert(name),
+        };
     }
+    Ok(items)
+}
+
+fn pick_ones_and_twos(items: &[&String]) -> Vec<BTreeSet<String>> {
+    let mut picked = Vec::new();
+    for (i, &it1) in items.iter().enumerate() {
+        picked.push([it1.clone()].into());
+        for &it2 in items.iter().skip(i + 1) {
+            picked.push([it1.clone(), it2.clone()].into());
+        }
+    }
+    picked
 }
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 enum Item {
     Microchip(String),
     Generator(String),
-}
-
-impl Item {
-    fn matches(&self, other: &Item) -> bool {
-        match (self, other) {
-            (Item::Microchip(name1), Item::Generator(name2)) => name1 == name2,
-            (Item::Generator(name1), Item::Microchip(name2)) => name1 == name2,
-            _ => false,
-        }
-    }
 }
 
 impl std::str::FromStr for Item {
