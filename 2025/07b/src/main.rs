9d0b65640660f9736405c41904f49e317a1338f1
John T. Wodder II <git@varonathe.org>
2025-12-07 10:42:51 -0500

    2025-07b: Use `Counter`

--- 2025/07b/src/main.rs
+++ 2025/07b/src/main.rs
@@ -1,6 +1,6 @@
 use adventutil::Input;
+use adventutil::counter::Counter;
 use adventutil::grid::Grid;
-use std::collections::HashMap;
 
 fn solve(input: Input) -> u64 {
     let grid = input.parse::<Grid<char>>();
@@ -8,20 +8,21 @@ fn solve(input: Input) -> u64 {
         .iter_cells()
         .find_map(|c| (*c == 'S').then(|| c.coords()))
         .unwrap();
-    let mut xes = HashMap::from([(start.x, 1u64)]);
+    let mut xes = Counter::new();
+    xes.add(start.x);
     for y in (start.y + 1)..(grid.height()) {
-        let mut new_xes = HashMap::new();
+        let mut new_xes = Counter::new();
         for (beam, qty) in xes {
             if grid[(y, beam)] == '^' {
-                *new_xes.entry(beam - 1).or_default() += qty;
-                *new_xes.entry(beam + 1).or_default() += qty;
+                new_xes.add_qty(beam - 1, qty);
+                new_xes.add_qty(beam + 1, qty);
             } else {
-                *new_xes.entry(beam).or_default() += qty;
+                new_xes.add_qty(beam, qty);
             }
         }
         xes = new_xes;
     }
-    xes.into_values().sum()
+    xes.total()
 }
 
 fn main() {
